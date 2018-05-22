8b9cd110982924c834d18ce13d5d3e3cf12357bb
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-05-22 20:25:26 +0200

    Added suits list to Suit object. Added gitignore file and sbt-related files and directories.

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "shell-poker"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
--- src/main/scala/gameEngine/core/cards/suits.scala
+++ src/main/scala/gameEngine/core/cards/suits.scala
@@ -1,6 +1,18 @@
 package main.scala.gameEngine.core.cards
 
 /** Represents card's suit. */
+object Suit {
+
+  /** All possible suits. */
+  val suits: List[Suit] = List(
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+  )
+}
+
+/** Represents particular suits. */
 sealed class Suit
 
 case object Clubs extends Suit
