1ed6e5200c3f4175ee29dcd790f021f7f81d181c
wujuu <patryk.zbigniew.wojtowicz@gmail.com>
2018-06-03 11:01:37 +0200

    player class changes

---
+++ src/main/scala/shellPoker/gameEngine/InvalidActionException.scala
@@ -0,0 +1,6 @@
+// package shellPoker.gameEngine
+
+// case class InvalidPokerHandException(message: String) extends Exception(message){
+
+//     def this(message: String) = this("Not a valid action!")
+// }
--- src/main/scala/shellPoker/gameEngine/Player.scala
+++ src/main/scala/shellPoker/gameEngine/Player.scala
@@ -2,11 +2,21 @@ package shellPoker.gameEngine
 
 import shellPoker.core.cards.Card
 
-class Player(var chipStack: ChipStack) {
-    var holeCards: Option[(Card, Card)] = None
-    var currentBet: ChipStack = new ChipStack(0)
+class Player(private var chipStack: ChipStack) {
+    private var holeCards: Option[(Card, Card)] = None
+    private var currentBet: ChipStack = new ChipStack(0)
 
-    def takeAction(input: String): Action = ???
+    def takeAction(input: List[String]): Action = {
+
+        input(0) match{
+            case "fold" => Fold
+            case "allin" => AllIn
+            case "check" => Check
+            case "call" => Call
+            case "bet" => Bet(input(1).toInt)
+        }
+
+    }
 
     def leave = ???
 }
