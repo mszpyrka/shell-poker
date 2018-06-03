c5b9785a228bc664599587c41c376c5cd5f4e2c4
wujuu <patryk.zbigniew.wojtowicz@gmail.com>
2018-06-03 19:16:09 +0200

    refactoring

---
+++ src/main/scala/shellPoker/gameEngine/GameManager.scala
@@ -0,0 +1,9 @@
+package shellPoker.gameEngine
+
+class GameManager{
+  val tableSeats = for(_ <- 0 until seatsAmount) yield new TableSeat
+  val dealer: Dealer = new Dealer
+  val communityCards: Option[List[Card]] = None
+  val pot: ChipStack = new ChipStack(0)
+  val positionManager: PositionManager = new PositionManager
+}
--- src/main/scala/shellPoker/gameEngine/InvalidActionException.scala
+++
@@ -1,6 +0,0 @@
-// package shellPoker.gameEngine
-
-// case class InvalidPokerHandException(message: String) extends Exception(message){
-
-//     def this(message: String) = this("Not a valid action!")
-// }
