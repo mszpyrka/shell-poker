ad91f60209df0defabde8969b273ffdb01ad003f
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-06-05 14:46:40 +0200

    Extened Player class.

--- src/main/scala/shellPoker/gameEngine/Player.scala
+++ src/main/scala/shellPoker/gameEngine/Player.scala
@@ -5,7 +5,33 @@ import shellPoker.core.cards.Card
 /*Represents a poker player
   *Responsible for storing player's game state
   */
-class Player(val initialChipStack: ChipStack) {
+class Player(val chipStack: ChipStack) {
+
   var holeCards: (Card, Card) = _
-  val currentBet: ChipStack = new ChipStack(0)
+  private var _currentBetSize: Int = 0
+  private var status: Status = IsActive
+
+  def isActive: Boolean = this.status == IsActive
+  def hasFolded: Boolean = this.status == HasFolded
+  def isAllIn: Boolean = this.status == IsAllIn
+
+  /* Returns the chip count in current player's bet. */
+  def currentBetSize: Int = _currentBetSize
+
+  /* Removes given chips amount from player's stack and adds it to player's bet. */
+  def makeBet(amount: Int): Unit = {
+
+    chipStack.removeChips(amount)
+    _currentBetSize += amount
+  }
+
+  /* Sets player's game status. */
+  def setStatus(status: Status): Unit = this.status = status
+
+  /* Private helper class for keeping track of current player's status. */
+  private class Status
+  case object IsActive extends Status
+  case object HasFolded extends Status
+  case object IsAllIn extends Status
+
 }
