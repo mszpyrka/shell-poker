9f0e26825fb2ff37eb96a755571209b87f468259
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-06-16 18:08:43 +0200

    Added createAndAddPlayer method.

--- src/main/scala/shellPoker/gameEngine/table/TableSeat.scala
+++ src/main/scala/shellPoker/gameEngine/table/TableSeat.scala
@@ -1,19 +1,20 @@
 package shellPoker.gameEngine.table
 
-import shellPoker.gameEngine.player.Player
+import shellPoker.gameEngine.player.{ChipStack, Player}
 
 /** Represent a single poker table seat.
   * Responsible for managing Player's presence on the seat
   */
 class TableSeat(val seatNumber: Int) {
+
   private var _player: Player = _
 
   def player: Player = _player
   
-  /*Checks if the seat is empty*/
+  /* Checks if the seat is empty. */
   def isEmpty: Boolean = _player == null
   
-  /*Adds Player instance to the seat*/
+  /* Adds Player instance to the seat. */
   def addPlayer(player: Player): Unit = {
 
     if (!this.isEmpty)
@@ -22,7 +23,21 @@ class TableSeat(val seatNumber: Int) {
     this._player = player
   }
 
-  /*Removes Players instance from the seat*/
+
+  /* Creates new Player instance and adds it to the seat. */
+  def createAndAddPlayer(name: String, initialChipCount: Int): Unit = {
+
+    if (!this.isEmpty)
+      throw InvalidSeatException("Seat is already taken.")
+
+    val newChipStack = new ChipStack(initialChipCount)
+    val newPlayer = new Player(name, this, newChipStack)
+
+    this._player = newPlayer
+  }
+
+
+  /* Removes Players instance from the seat. */
   def removePlayer(): Unit = {
 
     if(this.isEmpty)
