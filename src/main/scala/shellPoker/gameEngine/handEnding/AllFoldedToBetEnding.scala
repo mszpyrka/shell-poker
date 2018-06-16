40b798c9449ff5ece65ed00de773236326cec3ce
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-06-16 10:56:42 +0200

    Completed class implementation

--- src/main/scala/shellPoker/gameEngine/handEnding/AllFoldedToBetEnding.scala
+++ src/main/scala/shellPoker/gameEngine/handEnding/AllFoldedToBetEnding.scala
@@ -3,15 +3,19 @@ package shellPoker.gameEngine.handEnding
 import shellPoker.gameEngine.{PokerTable, Pot}
 
 /** Represents the type of ending when one player makes a bet and every other player folds.
-  * No final actions are needed to take in this case and the player that made the bet is awarded
-  * with all chips from the pot.
+  * The player that made the last bet mucks his cards, as no other player is in game.
+  * He is awarded with all the chips from the pot.
   *
   * @param table Table at which the game is played.
   */
 class AllFoldedToBetEnding(val table: PokerTable) extends HandEndingHelper(table, AllFoldedToBet) {
 
-  /* Does not take any actions. */
-  override def proceedWithFinalActions(): Unit = ()
+  /* Winning player mucks his cards. */
+  override def proceedWithFinalActions(): Unit = {
+
+    val winner = table.playersInHand.head
+    winner.muckCards()
+  }
 
   /* Gives whole pot to the only player in hand left. */
   override def calculateHandResults(): CompleteHandResults = {
