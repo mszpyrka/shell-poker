485871c098484c9d020eb28fe82fc3ec65bbf7eb
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-06-16 11:00:28 +0200

    Added helper shouldShowCards method.

--- src/main/scala/shellPoker/gameEngine/handEnding/ClassicShowdownEnding.scala
+++ src/main/scala/shellPoker/gameEngine/handEnding/ClassicShowdownEnding.scala
@@ -1,6 +1,6 @@
 package shellPoker.gameEngine.handEnding
 
-import shellPoker.gameEngine.PokerTable
+import shellPoker.gameEngine.{Player, PokerTable}
 
 /** Represents the type of ending when at least two players did not fold on the river.
   * Final actions in this case contain determining which players show / muck their cards.
@@ -12,4 +12,21 @@ class ClassicShowdownEnding(val table: PokerTable) extends HandEndingHelper(tabl
   override def proceedWithFinalActions(): Unit = ???
 
   override def calculateHandResults(): CompleteHandResults = ???
+
+  /* Checks if given player has any chance of winning any chips from the pot by showing his cards
+     after some other players have already revealed theirs. */
+  private def shouldShowCards(player: Player, showingPlayers: List[Player]): Boolean = {
+
+    for (pot <- table.potManager.pots if pot.entitledPlayers.contains(player)) {
+
+      // All potential winners of the pot
+      val rivals: List[Player] = pot.entitledPlayers.toSet.intersect(showingPlayers.toSet).toList
+
+      // If player has a chance of winning chips from any pot, then he should show cards.
+      if (PotDistributionHelper.hasBestHand(player, rivals, table.communityCards))
+        return true
+    }
+
+    false
+  }
 }
