d3c8ba058010da24c5a089bf6a6564e63455530e
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-06-17 20:14:18 +0200

    Minor fixes.

--- src/main/scala/shellPoker/gameEngine/player/PlayerCommunicator.scala
+++
@@ -1,9 +0,0 @@
-package shellPoker.gameEngine.player
-
-import shellPoker.core.cards.Card
-
-trait PlayerCommunicator {
-
-  def showCards(): Unit = ??? //playerActor ! ShowCards // to be implemented xD
-  def muckCards(): Unit = ??? //playerActor ! MuckCards  // this one as well LOL
-}
