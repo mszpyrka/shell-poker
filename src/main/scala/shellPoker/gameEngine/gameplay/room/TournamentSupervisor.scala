04df0520b98afd238c473717d632ed4c234ad9ee
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-06-17 20:09:21 +0200

    Deleted getHandSupervisor method.

--- src/main/scala/shellPoker/gameEngine/gameplay/room/TournamentSupervisor.scala
+++ src/main/scala/shellPoker/gameEngine/gameplay/room/TournamentSupervisor.scala
@@ -1,16 +1,15 @@
 package shellPoker.gameEngine.gameplay.room
 
 import shellPoker.gameEngine.gameplay.hand._
-import shellPoker.gameEngine.table._
 import shellPoker.gameEngine.gameplay._
 
 
 /* Responsible for running local test tournament style game. */
-class TournamentSupervisor(gameSettings: GameSettings) extends RoomSupervisor(gameSettings) {
+class TournamentSupervisor(
+    gameSettings: GameSettings,
+    communicator: HandSupervisorCommunicator
+  ) extends RoomSupervisor(gameSettings, communicator) {
 
   /** Gets players that joined the game during some hand. */
-  protected def getPendingPlayers(): List[PlayerId] = Nil
-
-  /** Gets chosen hand supervisor for this room supervisor. */
-  protected def getHandSupervisor(gameState: GameState): HandSupervisor = new LocalTestSupervisor(gameState)
+  protected def getPendingPlayers: List[PlayerId] = Nil
 }
