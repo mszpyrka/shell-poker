c171a25e71c17faf654eecad3d5a05660f357c29
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-06-17 20:12:32 +0200

    Added some necessary communication methods.

--- src/main/scala/shellPoker/gameEngine/gameplay/hand/HandSupervisorCommunicator.scala
+++ src/main/scala/shellPoker/gameEngine/gameplay/hand/HandSupervisorCommunicator.scala
@@ -1,15 +1,18 @@
 package shellPoker.gameEngine.gameplay.hand
 
 import shellPoker.gameEngine.gameplay.GameState
+import shellPoker.gameEngine.handEnding.CompleteHandResults
 import shellPoker.gameEngine.player.Player
 import shellPoker.gameEngine.playerAction.{Action, ActionValidation}
 
 /** Defines api that allows communication between HandSupervisor and outer world.
   */
-trait SupervisorCommunicator {
+trait HandSupervisorCommunicator {
 
+  def logGameStatus(gameState: GameState): Unit
   def logHandStatus(gameState: GameState): Unit
   def requestAction(player: Player): Action
   def logAction(player: Player, action: Action)
   def logActionValidation(player: Player, validation: ActionValidation)
+  def logHandResults(results: CompleteHandResults, gameState: GameState)
 }
