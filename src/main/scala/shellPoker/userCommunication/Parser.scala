db9ce5c919a1a10f4623c3159f9e18f11abe0a9c
wujuu <patryk.zbigniew.wojtowicz@gmail.com>
2018-06-17 17:11:22 +0200

    added chipstack to unicode

--- src/main/scala/shellPoker/userCommunication/Parser.scala
+++ src/main/scala/shellPoker/userCommunication/Parser.scala
@@ -3,6 +3,8 @@ package shellPoker.userCommunication
 import shellPoker.gameEngine._
 import shellPoker.core.cards._
 import shellPoker.gameEngine.playerAction._
+import shellPoker.gameEngine.player._
+import scala.util.Random
 
 object Parser {
     def stringToAction(userInput: String): Action = {
@@ -44,5 +46,34 @@ object Parser {
     }
   }
 
-  // def chipStackToUnicode()
+  def chipStackToUnicode(chipStack: ChipStack, maxChipCount: Int): String = {
+
+    val chipUnicodes: List[String] = List("\u26c0", "\u26c1", "\u26c2", "\u26c3")
+
+    var result: String = ""
+
+    val getRandomChipUnicode: (() => String) = () => Random.shuffle(chipUnicodes).head
+
+    val generateChipStack: (Int => String) = (stacksNum: Int) => {
+      var res: String = ""
+      for(_ <- 0 until stacksNum) res += getRandomChipUnicode()
+      res
+    }
+
+    if(chipStack.chipCount == 0) result += ""
+
+    else if (chipStack.chipCount < 0.1 * maxChipCount) 
+      result += generateChipStack(2)
+
+    else if (chipStack.chipCount >= 0.1 * maxChipCount && chipStack.chipCount < 0.5 * maxChipCount) 
+      result += generateChipStack(4)
+
+    else if (chipStack.chipCount >= 0.5 * maxChipCount && chipStack.chipCount <  maxChipCount) 
+      result += generateChipStack(8)
+
+    else if (chipStack.chipCount >= maxChipCount)
+      result += generateChipStack(16)
+
+    result
+  }
 }
